aa41b7d9413e6f8f3dafa747b9f6bec31e210d07
Nitin-cyberpunk <122776086+Nitin-cyperpunk@users.noreply.github.com>
2025-02-18 21:46:38 +0530

    first commit

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-<script>
+
 document.addEventListener('DOMContentLoaded', function () {
     const carousel = document.getElementById('carousel');
     const slides = carousel.getElementsByClassName('slide');
     const hero = document.getElementById('hero');
     let index = 0;
-    const totalSlides = slides.length / 2; // Because of duplicated slides
+    const totalSlides = slides.length; // Because of duplicated slides
 
     function updateBackground() {
         const currentSlide = slides[index];
         const imgSrc = currentSlide.querySelector('img').src;
-        hero.style.backgroundImage = `url('${./slideshow/Vector_2640.jpg"}')`;
+        hero.style.backgroundImage = `url('${imgSrc}')`;
+        console.log(`Background updated to: ${imgSrc}`); // Debugging line
     }
 
     function nextSlide() {
@@ -19,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
         updateBackground();
     }
 
-    setInterval(nextSlide, 3000); // Change slide every 3 seconds
+    setInterval(nextSlide, 4000); // Change slide every 4 seconds
     updateBackground(); // Initial background update
 });
-</script>
+
