85cdef1c7955fe3e7f609c78a8e12ceeab423b23
Nitin-cyberpunk <122776086+Nitin-cyperpunk@users.noreply.github.com>
2025-03-28 01:49:44 +0530

    first commit

--- script.js
+++ script.js
@@ -99,16 +99,30 @@ document.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('resize', checkScreenWidth);
 });
 
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper('.mySwiper', {
     slidesPerView: 1,
-    spaceBetween: 30,
+    spaceBetween: 15,
     loop: true,
     pagination: {
-      el: ".swiper-pagination",
+      el: '.swiper-pagination',
       clickable: true,
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
     },
   });
